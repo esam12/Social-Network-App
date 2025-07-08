2b72bd60a0749b9efdfa281f688d12b69439adec
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-09 01:43:26 +0300

    Extend color palette with new background and neutral shades

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -6,4 +6,18 @@ class AppColors {
   static Color onSurface = Color(0xFF8F8F8F);
   static Color surface = Color(0xFFFFFFFF);
   static Color text = Color(0xFF0E103F);
+
+  // Background colors
+  static const Color light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color primaryBackground = Color(0xFFF3F5FF);
+
+  // Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
 }
