e6f7fe2aa445f80a26b866b3428f1e7a1bcbf398
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-11 23:56:28 +0300

    refactor: move get_it setup to services folder

---
+++ lib/core/services/get_it.dart
@@ -0,0 +1,44 @@
+import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:social_network_app/core/api/api_client.dart';
+import 'package:social_network_app/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:social_network_app/features/auth/data/repository/user_repository_imp.dart';
+import 'package:social_network_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:social_network_app/features/auth/domain/repository/user_repository.dart';
+import 'package:social_network_app/features/auth/presentation/manager/user_bloc/user_bloc.dart';
+
+var getIt = GetIt.instance;
+
+void setupGetIt() {
+  // Register google sign in
+  registerGoogleSignIn();
+  // Register api client
+  registerApiClient();
+  // Register repositories
+  registerRepositories();
+  // Register User bloc
+  registerUserBloc();
+}
+
+void registerGoogleSignIn() {
+  getIt.registerSingleton<GoogleSignIn>(GoogleSignIn());
+}
+
+void registerApiClient() {
+  getIt.registerSingleton(ApiClient());
+}
+
+void registerRepositories() {
+  getIt.registerSingleton<AuthRepository>(
+    AuthRepositoryImpl(googleSignIn: getIt()),
+  );
+  getIt.registerSingleton<UserRepository>(
+    UserRepositoryImp(userRemoteDatasource: getIt()),
+  );
+}
+
+void registerUserBloc() {
+  getIt.registerFactory<UserBloc>(
+    () => UserBloc(authRepository: getIt(), userRepository: getIt()),
+  );
+}
