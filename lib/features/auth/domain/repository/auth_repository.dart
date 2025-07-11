8657bb0ed76fbc75f29e4bf8ea01d98e403e8a21
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-11 23:56:14 +0300

    feat: add new user data methods to AuthRepository interface

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -4,5 +4,16 @@ import 'package:social_network_app/features/auth/domain/entity/user_entity.dart'
 
 abstract class AuthRepository {
   Future<Either<Failure, UserEntity>> signInWithGoogle();
+
+  /// Add User Data
+  Future addUserData({required UserEntity user});
+
+  /// Save User Data
+  Future saveUserData({required UserEntity user});
+
+  /// Get User Data
+  Future<UserEntity> getUserData({required String uid});
+
+  /// Log Out
   Future<Either<Failure, void>> logOut();
 }
