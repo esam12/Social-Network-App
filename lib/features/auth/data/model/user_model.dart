e930a84853f54358a049ccfbcb90308cc1ac6f68
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-12 23:26:59 +0300

    Implement UserModel with Firebase integration

--- lib/features/auth/data/model/user_model.dart
+++ lib/features/auth/data/model/user_model.dart
@@ -1,3 +1,63 @@
-class UserModel {
-  
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_network_app/features/auth/domain/entity/user_entity.dart';
+
+class UserModel extends UserEntity {
+  const UserModel({
+    required super.id,
+    required super.name,
+    required super.email,
+    required super.avatar,
+    super.bio,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['uid'] ?? '',
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      avatar: json['avatar'] ?? '',
+      bio: json['bio'],
+    );
+  }
+
+  factory UserModel.fromFirebaseUser(User user) {
+    return UserModel(
+      id: user.uid,
+      name: user.displayName ?? '',
+      email: user.email ?? '',
+      avatar: user.photoURL ?? '',
+    );
+  }
+
+  factory UserModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data()!;
+    return UserModel(
+      id: doc.id,
+      name: data['name'] ?? '',
+      email: data['email'] ?? '',
+      avatar: data['avatar'] ?? '',
+      bio: data['bio'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': id,
+      'name': name,
+      'email': email,
+      'avatar': avatar,
+      'bio': bio,
+    };
+  }
+
+  factory UserModel.fromEntity(UserEntity user) {
+    return UserModel(
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      bio: user.bio,
+    );
+  }
 }
