1d5fcb78e03e9d6ae109d00ee8581095b224f0e1
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-17 16:11:23 +0300

    refactor: Update ProfileState to use UserEntity

--- lib/features/profile/presentation/manager/cubit/profile_state.dart
+++ lib/features/profile/presentation/manager/cubit/profile_state.dart
@@ -1,54 +1,46 @@
-
-import 'dart:io';
-
 import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:social_network_app/features/auth/domain/entity/user_entity.dart';
 
 enum ProfileStatus { initial, loading, success, error }
 
 class ProfileState extends Equatable {
-  final String userId;
-  final String name;
-  final String bio;
-  final File? imageFile;
+  final UserEntity user;
+  // final XFile? imageFile;
   final ProfileStatus status;
   final String? errorMessage;
 
   const ProfileState({
-    required this.userId,
-    required this.name,
-    required this.bio,
-    required this.imageFile,
+    required this.user,
+    // required this.imageFile,
     required this.status,
     this.errorMessage,
   });
 
   factory ProfileState.initial() => ProfileState(
-    userId: '',
-    name: '',
-    bio: '',
-    imageFile: null,
+    user: UserEntity.empty(),
+    // imageFile: null,
     status: ProfileStatus.initial,
     errorMessage: null,
   );
 
   ProfileState copyWith({
-    String? userId,
-    String? name,
-    String? bio,
-    File? imageFile,
+    UserEntity? user,
+    XFile? imageFile,
     ProfileStatus? status,
     String? errorMessage,
   }) {
     return ProfileState(
-      userId: userId ?? this.userId,
-      name: name ?? this.name,
-      bio: bio ?? this.bio,
-      imageFile: imageFile ?? this.imageFile,
+      user: user ?? this.user,
+      // imageFile: imageFile ?? this.imageFile,
       status: status ?? this.status,
       errorMessage: errorMessage,
     );
   }
 
   @override
-  List<Object?> get props => [userId, name, bio, imageFile?.path, status, errorMessage];
+  List<Object?> get props => [
+    user, // imageFile?.path,
+    status, errorMessage,
+  ];
 }
