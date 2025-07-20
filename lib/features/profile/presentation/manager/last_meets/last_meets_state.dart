349c63a2b0f57baed26911c683ea31d745281470
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-20 17:34:41 +0300

    Create LastMeetsState with status management and pagination

---
+++ lib/features/profile/presentation/manager/last_meets/last_meets_state.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+import 'package:social_network_app/features/meet/domain/entity/meet_entity.dart';
+
+enum LastMeetsStatus { initial, loading, success, error }
+
+class LastMeetsState extends Equatable {
+  final LastMeetsStatus status;
+  final String? errorMessage;
+
+  final List<MeetEntity>? lastMeets;
+  final int currentPage;
+  final bool isLastPage;
+
+  LastMeetsState._({
+    required this.status,
+    this.errorMessage,
+    this.lastMeets,
+    this.currentPage = 1,
+    this.isLastPage = false,
+  });
+
+  factory LastMeetsState.initial() =>
+      LastMeetsState._(status: LastMeetsStatus.initial);
+
+  LastMeetsState copyWith({
+    LastMeetsStatus? status,
+    String? errorMessage,
+    List<MeetEntity>? lastMeets,
+    int? currentPage,
+    bool? isLastPage,
+  }) {
+    return LastMeetsState._(
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+      lastMeets: lastMeets ?? this.lastMeets,
+      currentPage: currentPage ?? this.currentPage,
+      isLastPage: isLastPage ?? this.isLastPage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+    status,
+    errorMessage,
+    lastMeets,
+    currentPage,
+    isLastPage,
+  ];
+}
