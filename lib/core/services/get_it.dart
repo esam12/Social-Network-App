3e6213da26e0857ff2780023d5558438ca99a2cf
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-20 17:31:36 +0300

    Register MeetRepository and LastMeetsBoc in GetIt service locator

--- lib/core/services/get_it.dart
+++ lib/core/services/get_it.dart
@@ -5,9 +5,12 @@ import 'package:social_network_app/core/services/firestore_service.dart';
 import 'package:social_network_app/features/auth/data/repository/auth_repository_impl.dart';
 import 'package:social_network_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:social_network_app/features/auth/presentation/manager/user_bloc/user_bloc.dart';
+import 'package:social_network_app/features/meet/data/repository/meet_repository_impl.dart';
+import 'package:social_network_app/features/meet/domain/repository/meet_repository.dart';
 import 'package:social_network_app/features/profile/data/repository/profile_repository_impl.dart';
 import 'package:social_network_app/features/profile/domain/repository/profile_repository.dart';
 import 'package:social_network_app/features/profile/presentation/manager/cubit/profile_cubit.dart';
+import 'package:social_network_app/features/profile/presentation/manager/last_meets/last_meets_boc.dart';
 
 var getIt = GetIt.instance;
 
@@ -32,12 +35,14 @@ void registerRepositories() {
   getIt.registerSingleton<AuthRepository>(AuthRepositoryImpl(getIt(), getIt()));
 
   getIt.registerSingleton<ProfileRepository>(ProfileRepositoryImpl(getIt()));
+
+  getIt.registerSingleton<MeetRepository>(MeetRepositoryImpl(dbService: getIt()));
 }
 
 void registerUserBloc() {
   getIt.registerFactory<UserBloc>(() => UserBloc(authRepository: getIt()));
 
-  getIt.registerFactory<ProfileCubit>(
-    () => ProfileCubit(repository: getIt()),
-  );
+  getIt.registerFactory<ProfileCubit>(() => ProfileCubit(repository: getIt()));
+
+  getIt.registerFactory(() => LastMeetsBoc(repository: getIt()));
 }
