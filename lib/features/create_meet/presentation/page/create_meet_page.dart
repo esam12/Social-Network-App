ddb211793e2d5b747f3757b1e5815f2dbbe85793
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-22 20:21:37 +0300

    Implement CreateMeetBloc in create meet page with state management

--- lib/features/create_meet/presentation/page/create_meet_page.dart
+++ lib/features/create_meet/presentation/page/create_meet_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:social_network_app/core/common/widgets/default_button.dart';
 import 'package:social_network_app/core/common/widgets/default_text_form_field.dart';
+import 'package:social_network_app/core/services/get_it.dart';
+import 'package:social_network_app/features/create_meet/presentation/manager/create_meet/create_meet_bloc.dart';
+import 'package:social_network_app/features/create_meet/presentation/manager/create_meet/create_meet_event.dart';
+import 'package:social_network_app/features/create_meet/presentation/manager/create_meet/create_meet_state.dart';
 import 'package:social_network_app/features/create_meet/presentation/page/location_picker_page.dart';
 
 class CreateMeetPage extends StatefulWidget {
@@ -25,102 +30,150 @@ class _CreateMeetPageState extends State<CreateMeetPage> {
   void dispose() {
     _titleController.dispose();
     _descriptionController.dispose();
-    _mapController?.dispose();  
+    _mapController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Create Meet',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Title',
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 10),
-                DefaultTextFormField(
-                  hintText: "Enter meet title",
-                  controller: _titleController,
-                ),
-
-                SizedBox(height: 15),
-                Text(
-                  'Description',
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 10),
-                DefaultTextFormField(
-                  hintText: "Enter meet description",
-                  controller: _descriptionController,
-                  maxLength: 255,
-                  minLines: 2,
-                  maxLines: 6,
-                ),
-
-                SizedBox(height: 15),
-
-                Text(
-                  "Time",
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 5.0),
-                Text(
-                  "Events automatically mark as completed 2 hours after that start.",
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    fontSize: 12,
-                    color: Theme.of(
-                      context,
-                    ).colorScheme.onSurface.withValues(alpha: 0.7),
-                  ),
-                ),
-                SizedBox(height: 10),
-                _buildTimePicker(context),
-                SizedBox(height: 15),
-                Text(
-                  "Location",
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  "Tap map to select location",
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    fontSize: 12,
-                    color: Theme.of(
-                      context,
-                    ).colorScheme.onSurface.withValues(alpha: 0.7),
-                  ),
-                ),
-                SizedBox(height: 10),
-                _buildLocationPicker(),
-                SizedBox(height: 10),
-                DefaultButton(title: 'Create Meet', onPressed: () {}),
-              ],
-            ),
+    return BlocProvider(
+      create: (context) => getIt<CreateMeetBloc>(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Create Meet',
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
+        ),
+        body: BlocConsumer<CreateMeetBloc, CreateMeetState>(
+          listener: (context, state) {
+            if (state.status == CreateMeetStatus.error) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.errorMessage ?? '')));
+            }
+          },
+          builder: (context, state) {
+            if (state.status == CreateMeetStatus.loading) {
+              return Scaffold(
+                body: Center(child: CircularProgressIndicator.adaptive()),
+              );
+            }
+            if (state.status == CreateMeetStatus.success) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Meet created successfully')),
+              );
+            }
+
+            return SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Title',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      DefaultTextFormField(
+                        hintText: "Enter meet title",
+                        controller: _titleController,
+                        onChanged: (_) {
+                          setState(() {});
+                        },
+                      ),
+
+                      SizedBox(height: 15),
+                      Text(
+                        'Description',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      DefaultTextFormField(
+                        hintText: "Enter meet description",
+                        controller: _descriptionController,
+                        maxLength: 255,
+                        minLines: 2,
+                        maxLines: 6,
+                        onChanged: (_) {
+                          setState(() {});
+                        },
+                      ),
+
+                      SizedBox(height: 15),
+
+                      Text(
+                        "Time",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 5.0),
+                      Text(
+                        "Events automatically mark as completed 2 hours after that start.",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 12,
+                          color: Theme.of(
+                            context,
+                          ).colorScheme.onSurface.withValues(alpha: 0.7),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      _buildTimePicker(context),
+                      SizedBox(height: 15),
+                      Text(
+                        "Location",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Tap map to select location",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 12,
+                          color: Theme.of(
+                            context,
+                          ).colorScheme.onSurface.withValues(alpha: 0.7),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      _buildLocationPicker(),
+                      SizedBox(height: 10),
+                      DefaultButton(
+                        title: 'Create Meet',
+                        onPressed:
+                            location == null ||
+                                _titleController.text.isEmpty ||
+                                _descriptionController.text.isEmpty
+                            ? null
+                            : () {
+                                context.read<CreateMeetBloc>().add(
+                                  CreateMeetEvent(
+                                    title: _titleController.text,
+                                    description: _descriptionController.text,
+                                    time: timeOfDay,
+                                    location: location!,
+                                  ),
+                                );
+                              },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -212,7 +265,7 @@ class _CreateMeetPageState extends State<CreateMeetPage> {
             : {},
 
         initialCameraPosition: CameraPosition(
-          target: LatLng(36.542841, 36.150188),
+          target: location ?? LatLng(36.542841, 36.150188),
           zoom: 10,
         ),
       ),
