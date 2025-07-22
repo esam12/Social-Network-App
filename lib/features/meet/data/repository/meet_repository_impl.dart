0bd6ee3217156f63ce8d7a327dc6e1ad2ee5acf9
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-22 20:21:57 +0300

    Implement createMeet method in MeetRepositoryImpl

--- lib/features/meet/data/repository/meet_repository_impl.dart
+++ lib/features/meet/data/repository/meet_repository_impl.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:social_network_app/core/errors/failures.dart';
 import 'package:social_network_app/core/services/data_service.dart';
 import 'package:social_network_app/core/utils/backend_endpoint.dart';
@@ -16,7 +22,64 @@ class MeetRepositoryImpl implements MeetRepository {
     int? limit,
   }) async {
     try {
-      return right(await dbService.getData( path: BackendEndpoint.getLastMeets, query: {'limit': limit}));
+      final result = await dbService.getData(
+        path: BackendEndpoint.getLastMeets,
+        query: {if (limit != null) 'limit': limit},
+      );
+
+      final data = (result as List<dynamic>)
+          .cast<Map<String, dynamic>>()
+          .map(MeetEntity.fromJson)
+          .toList();
+      log(data.toString());
+
+      return right(data);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> createMeet({
+    required String title,
+    required String description,
+    required TimeOfDay time,
+    required LatLng location,
+  }) async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        return left(ServerFailure("User not logged in"));
+      }
+
+      final adminData = {
+        'id': user.uid,
+        'name': user.displayName ?? '',
+        'email': user.email ?? '',
+        'avatar': user.photoURL ?? '',
+      };
+      final docRef = FirebaseFirestore.instance.collection('meets').doc();
+    final generatedId = docRef.id;
+      return right(
+        dbService.addData(
+          path: BackendEndpoint.createMeet,
+          data: {
+            'id': generatedId,
+            'title': title,
+            'description': description,
+            'time': DateTime.now()
+                .copyWith(hour: time.hour, minute: time.minute)
+                .toUtc()
+                .toIso8601String(),
+            'latitude': location.latitude,
+            'longitude': location.longitude,
+            'admin': adminData,
+            'attendees': [adminData],
+            'isFinished': false,
+            'isCancelled': false,
+          },
+        ),
+      );
     } catch (e) {
       return left(ServerFailure(e.toString()));
     }
