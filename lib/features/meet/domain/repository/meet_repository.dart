1f45bcb4f61f22b13fbf1aaca116377425a1fdf8
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-22 20:22:43 +0300

    Add createMeet method to MeetRepository interface

--- lib/features/meet/domain/repository/meet_repository.dart
+++ lib/features/meet/domain/repository/meet_repository.dart
@@ -1,4 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:social_network_app/core/errors/failures.dart';
 import 'package:social_network_app/features/meet/domain/entity/meet_entity.dart';
 
@@ -6,5 +8,12 @@ abstract class MeetRepository {
   Future<Either<Failure, List<MeetEntity>>> getLastMeets({
     int? page,
     int? limit,
+  });
+
+  Future<Either<Failure, void>> createMeet({
+    required String title,
+    required String description,
+    required TimeOfDay time,
+    required LatLng location,
   });
 }
